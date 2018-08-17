d5827078b90498d35702480f8490a39a5160d238
NahuBocco <nahuelbocco@gmail.com>
2018-08-17 19:49:46 -0300

    19:49

---
+++ conexxion.php
@@ -0,0 +1,8 @@
+<?php
+	$dbhost="localhost";
+	$dbuser="root";
+	$dbpass="93256123";
+	$db="vinos";
+	$conexxion=mysql_connect("$dbhost","$dbuser","$dbpass");
+	mysql_select_db("$db");
+?>
---
+++ index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Philosopher|Playball|Sacramento|Lovers+Quarrel|Sue+Ellen+Francisco" rel="stylesheet">
+    <style>
+      body {
+          background-color: black;
+      }
+      </style>
+
+    <title>Vinos</title>
+  </head>
+  <body>
+
+    <div class="parallax no-margin" id="inicio">
+      <div class="container2" style="z-index: 1">
+        <div class="topnav letra_barra" >
+          <a href="#about">NOSOTROS</a>
+          <a href="#news">CONTACTANOS</a>
+          <a href="#contact">NUESTROS PRODUCTOS</a>
+          <div class="topnav_left letra_barra">
+            <a href="#inicio" class="logo" style="z-index: -1 !important;">Vinos Oscar</a>
+          </div>
+        </div>
+        
+      </div>
+      <h1 class="titulo">Vinos Oscar</h1>
+
+      <div style="width: 60%;margin: auto;">
+        <h4 class="slogan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a faucibus diam, quis hendrerit nisi. Duis imperdiet et tellus vel blandit. Donec faucibus, neques.</h4>
+      </div>
+      
+    </div>
+
+    <div id="demo" class="carousel slide no-margin" data-ride="carousel">
+
+      <!-- Indicators -->
+      <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+      </ul>
+      
+      <!-- The slideshow -->
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/vino1.jpg">
+        </div>
+        <div class="carousel-item">
+          <img src="img/vino2.jpg">
+        </div>
+        <div class="carousel-item">
+          <img src="img/vino3.jpg">
+        </div>
+      </div>
+      
+      <!-- Left and right controls -->
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
+
+    <div class="row">
+        <img class="opacidad col-lg-6" src="img/vino4.jpg">
+        <img class="opacidad col-lg-6" src="img/vino5.jpg">
+      
+
+    </div>
+
+
+
+
+  
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
