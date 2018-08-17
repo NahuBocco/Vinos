c969209e6223d8ccdf4b911af37a9c1f37089dee
NahuBocco <nahuelbocco@gmail.com>
2018-08-17 20:12:41 -0300

    20:12

--- index.php
+++ index.php
@@ -71,8 +71,8 @@
     </div>
 
     <div class="row">
-        <img class="opacidad col-lg-6" src="img/vino4.jpg">
-        <img class="opacidad col-lg-6" src="img/vino5.jpg">
+        <img class="opacidad col-md-6" src="img/vino4.jpg">
+        <img class="opacidad col-md-6" src="img/vino5.jpg">
     </div>
 
 
