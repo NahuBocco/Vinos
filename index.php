1156ef0cc802cf402aaa237e258ad938f1baaa48
sofiabmm <sbmartinezmartinez@gmail.com>
2018-08-19 20:32:00 -0300

    831

--- index.php
+++ index.php
@@ -66,9 +66,9 @@
     </div>
   -->
     
-<div class="responsive">
+  <div class="responsive">
       <div class="gallery">
-        <a target="_blank">
+        <a>
           <img src="img/catalogo.jpeg">
         </a>
         <div class="desc">Add a description of the image here</div>
@@ -77,7 +77,7 @@
 
     <div class="responsive">
       <div class="gallery">
-        <a target="_blank">
+        <a>
           <img src="img/catalogo.jpeg">
         </a>
         <div class="desc">Add a description of the image here</div>
@@ -86,7 +86,7 @@
 
     <div class="responsive">
       <div class="gallery">
-        <a target="_blank">
+        <a>
           <img src="img/catalogo.jpeg">
         </a>
         <div class="desc">Add a description of the image here</div>
@@ -94,13 +94,13 @@
     </div>
     <div class="responsive">
       <div class="gallery">
-        <a target="_blank">
+        <a>
           <img src="img/catalogo.jpeg">
         </a>
         <div class="desc">Add a description of the image here</div>
       </div>
     </div>
-      
+  </div>
 
     <div class="row opacidad">
         <img class="opacidad col-md-6" src="img/vino4.jpg">
