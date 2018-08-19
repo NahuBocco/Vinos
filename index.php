375029f44e474a960163cee15c21be3d9ed1386a
sofiabmm <sbmartinezmartinez@gmail.com>
2018-08-19 16:33:22 -0300

    4:33

--- index.php
+++ index.php
@@ -39,17 +39,17 @@
       
     </div>
 
-    <div id="demo" class="carousel slide no-margin" data-ride="carousel">
+    <!--<div id="demo" class="carousel slide no-margin" data-ride="carousel">-->
 
       <!-- Indicators -->
-      <ul class="carousel-indicators">
+      <!--<ul class="carousel-indicators">
         <li data-target="#demo" data-slide-to="0" class="active"></li>
         <li data-target="#demo" data-slide-to="1"></li>
         <li data-target="#demo" data-slide-to="2"></li>
       </ul>
-      
+      -->
       <!-- The slideshow -->
-      <div class="carousel-inner">
+      <!--<div class="carousel-inner">
         <div class="carousel-item active">
           <img src="img/vino1.jpg">
         </div>
@@ -60,17 +60,34 @@
           <img src="img/vino3.jpg">
         </div>
       </div>
-      
+      -->
       <!-- Left and right controls -->
-      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <!--<a class="carousel-control-prev" href="#demo" data-slide="prev">
         <span class="carousel-control-prev-icon"></span>
       </a>
       <a class="carousel-control-next" href="#demo" data-slide="next">
         <span class="carousel-control-next-icon"></span>
       </a>
     </div>
+  -->
+    <div class="contenedor ul">
+      <ul>
+        <li>
+          <img src="img/catalogo.jpeg">
+        </li>
+        <li>
+          <img src="img/catalogo.jpeg">
+        </li>
+        <li>
+          <img src="img/catalogo.jpeg">
+        </li>
+      </ul>
 
-    <div class="row">
+    </div>
+
+      
+
+    <div class="row no-margin">
         <img class="opacidad col-md-6" src="img/vino4.jpg">
         <img class="opacidad col-md-6" src="img/vino5.jpg">
     </div>
